7ccae56307447962cce6c49cae78f17356831d46
shabazabbass <shabazabbass4@gmail.com>
2024-11-28 21:25:58 +0530

    Initial programs

--- Programs/Process/1.c
+++ Programs/Process/1.c
@@ -1,19 +1,23 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<sys/types.h>
+#include<string.h>
 #include<errno.h>
 int main(){
 	pid_t pid;
 	pid = fork();
 	if(pid == -1){
-		printf("fork has failed due to this reason and the error code is %d\n",errno);
+		printf("Error Description: %s \n",strerror(errno));
+		perror("Fork Failed");
 		return errno;
 	}
 	if(pid == 0){
+		perror("Child Process: ");
 		printf("child process is running\n");
 		//return 0;
 	}
 	else{
+		perror("Parent Process: ");
 		printf("parent process is running\n");
 		//return 0;
 	}

81b75c40b36e4acf13cd0994965023b10e8f973a
shabazabbass <shabazabbass4@gmail.com>
2024-11-28 21:25:58 +0530

    Initial programs

--- Programs/Process/3.c
+++ Programs/Process/3.c
@@ -1,19 +1,37 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<unistd.h>
 #include<sys/types.h>
+#include<errno.h>
 int main(){
-	printf("parent process id : %d \n",getpid());
-	if(fork() == 0){
-		printf("child process id : %d parent process id : %d \n",getpid(),getppid());
+	pid_t pid;
+	pid = getpid();
+	printf("parent process id : %u\n",pid);
+	pid = fork();
+	if(pid == 0){
+		perror("child 1 creation  ");
+		printf("child 1 pid : %u parent pid %u \n ",getpid(),getppid());
+		return EXIT_SUCCESS;
 	}
-	else{
-		if(fork() == 0){
-			printf("child process id : %d parent process id : %d \n",getpid(),getppid());
+	else if(pid > 0){
+		pid  = fork();
+		if(pid ==  0){
+			perror("child 2 creation  ");
+			printf("child 2 pid : %u parent pid %u \n ",getpid(),getppid());
+			return EXIT_SUCCESS;	
+		}
+		else if(pid >  0) {
+//			sleep(10);
+			return EXIT_SUCCESS;
 		}
 		else{
-			;
+			perror("child 2 creation ");
+			return errno;
 		}
-		while(1);
+	
+	}
+	else{
+		perror("child 1 creation : ");
+		return errno ;
 	}
 }
-
